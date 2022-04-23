5f0f484bae0fa7e00eec86257a853715649fe257
RUTVIK-SANATHARA <rutvik.sanathara@gmail.com>
2022-04-23 19:20:01 +0530

    second times

--- src/components/sub.jsx
+++ src/components/sub.jsx
@@ -33,11 +33,25 @@ const Sub = () => {
                         state.map((ele) => {
                             return (
                                 <>
-                                    <div className="col-12 col-md-6">
+                                    <div className="col-12 col-md-6 pe-5">
                                         <img src={ele.flags.svg} alt="flags" style={{ width: "100%", height: "500px" }} />
                                     </div>
-                                    <div className="col-12 col-md-6">
-
+                                    <div className="col-12 col-md-6 px-5 pt-5">
+                                           <div className="row">
+                                               <div className="col-12 col-md-6">
+                                                  <h1>{ele.name}</h1>
+                                                  <p className="pt-4"><span className="fw-bold">Native Name: </span>{ele.nativeName}</p>
+                                                  <p className="pt-4"><span className="fw-bold">Population: </span> {ele.population}</p>
+                                                  <p className="pt-4"><span className="fw-bold">Region: </span>{ele.region}</p>
+                                                  <p className="pt-4"><span className="fw-bold">Sub Region: </span> {ele.subregion}</p>
+                                                  <p className="pt-4"><span className="fw-bold">Capital: </span>{ele.capital}</p>
+                                               </div>
+                                               <div className="col-12 col-md-6 pt-5">
+                                                <p className="pt-4"><span className="fw-bold">Top Level Domain: </span>{ele.topLevelDomain}</p>
+                                                <p className="pt-4"><span className="fw-bold">Currencies: </span>{ele.currencies[0].code}</p>
+                                                <p className="pt-4"><span className="fw-bold">Languages: </span>{ele.languages[0].name}</p>
+                                               </div>
+                                           </div>
                                     </div>
                                 </>
                             )
